7da46bbdcd9663a323b86abae262e6c2b4d39842
Dimitrios Chatzinikolis <28847306+dchatzin@users.noreply.github.com>
2022-06-28 19:13:20 -0400

    Update setup.py
    Correct version of pymanopt because its latest updates are not yet reflected to morphomatics source code

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
   install_requires=[
           'numpy',
           'scipy',
-          'pymanopt=0.2.5',
+          'pymanopt==0.2.5',
           'joblib',
           'pyvista>=0.25',
           'pyvistaqt'
