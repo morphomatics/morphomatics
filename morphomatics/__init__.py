1321dcf70d845805192f9f4ddb5be4ddfc8044cb
tycowicz <cvt@tycowicz.de>
2025-07-12 10:54:29 +0200

    fix version in __init__.py

--- morphomatics/__init__.py
+++ morphomatics/__init__.py
@@ -3,11 +3,11 @@
 #   This file is part of the Morphomatics library                              #
 #       see https://github.com/morphomatics/morphomatics                       #
 #                                                                              #
-#   Copyright (C) 2024 Zuse Institute Berlin                                   #
+#   Copyright (C) 2025 Zuse Institute Berlin                                   #
 #                                                                              #
 #   Morphomatics is distributed under the terms of the MIT License.            #
 #       see $MORPHOMATICS/LICENSE                                              #
 #                                                                              #
 ################################################################################
 
-__version__ = '4.1'
+__version__ = '4.1.1'
